142af2816300b79a2fb4eb463e9954d782912757
Srinath <srinath@iambot.net>
2014-08-21 21:37:17 +0530

    Added a raw query example

--- example/migrations/0002_tweet_user.py
+++ example/migrations/0002_tweet_user.py
@@ -2,13 +2,22 @@ from example.models import User
 from peewee import ForeignKeyField
 
 tweet_user = ForeignKeyField(User, to_field=User.id, db_column="user_id", null=True)
+tweet_view = '''
+CREATE OR REPLACE VIEW user_tweets_view AS
+SELECT users.id, users.name, users.email, tweet.text
+FROM users
+LEFT JOIN tweet ON tweet.user_id = users.id
+ORDER BY tweet.id DESC;
+'''
 
 def up(migrator):
 	migrator(
 		migrator.add_column('tweet', 'user_id', tweet_user),
+		migrator.raw_query(tweet_view),
 	)
 
 def down(migrator):
 	migrator(
 		migrator.drop_column('tweet', 'user_id'),
+		migrator.raw_query('DROP VIEW user_tweets_view'),
 	)
